ef04a53ed5669b384f30c2164e2732fb10c28573
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2023-04-02 22:42:09 +0200

    getCustomers finished

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -15,7 +15,11 @@ public class Database {
 	 * Modify it to fit your environment and then use this string when connecting to your database!
 	 */
 	//private static final String jdbcString = "jdbc:mysql://localhost/krusty";
-	private static final String jdbcString = "jdbc:mysql://localhost:3306/krusty?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+	private static final String jdbcString = "jdbc:mysql://localhost:3306/krusty?" +
+			"useSSL=false" +
+			"&allowPublicKeyRetrieval=true" +
+			"&serverTimezone=UTC" +
+			"&database=krusty";
 
 	// For use with MySQL or PostgreSQL
 	private static final String jdbcUsername = "root";
